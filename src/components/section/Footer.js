8904933b1ad294ec780770ae78f52402edc7a8d6
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-02 11:07:14 +0100

    Update

---
+++ src/components/section/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ckasses from './Fotter.module.css';
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/section/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
