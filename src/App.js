9b2fb3392de081c76db6fd8691f66d4348f88f20
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-02 11:21:55 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import './App.css';
+import { Fragment } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+   <Fragment>
+     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+     </Switch>
+   </Fragment>
   );
 }
 
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutUs = () => {
     return (
         <div>
-            
+            <h1>About Us Pagw</h1>
         </div>
     )
 }
