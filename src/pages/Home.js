94f63116a41819dfab1365963478c415a6baa98c
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-02 19:10:05 +0100

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import backgroundImg from '../assets/pizza.jpeg';
 import classes from './Home.module.css';
 
 const Home = () => {
     return (
-        <div className={classes.headerContainer}>
-            <h1>Home Page</h1>
+        <div className={classes.home} style={{backgroundImage: `url(${backgroundImg})`}}>
+             
+            <div className={classes.headerContainer}>
+                <h1>Giovanni Pizzeria</h1>
+                <p>Pizza with Italian taste</p>
+                <Link to="/menu"><button>Order Now</button></Link>
+            </div>
         </div>
     )
 }
