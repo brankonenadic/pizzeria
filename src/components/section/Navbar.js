4a62159af1528713504f637ac0e65a43f51655dd
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-02 17:05:17 +0100

    Update

--- src/components/section/Navbar.js
+++ src/components/section/Navbar.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Navbar.module.css';
 import logo from '../../assets/pizzaLogo.png';
 import { Link, NavLink } from 'react-router-dom';
 import { Fragment } from 'react/cjs/react.production.min';
 
 const Navbar = () => {
-    
+    const [openNav, setopenNav] = useState(false);
+
+    const openNavHandler = () => {
+        setopenNav(!openNav);
+    };
+    const closeMenu = () => {
+        setopenNav(false);
+    }
     return (
         <Fragment>
             <div className={classes.navbar}>
                 <div className={classes.leftSide}>
-                    <Link to="/home"><img src={logo} alt="logo" /> </Link>
+                    <Link to="/home"><img src={logo} alt="logo"  onClick={closeMenu}/> </Link>
                 </div>
                 <div className={classes.rightSide}>
-                    <ul>
-                        <li><NavLink to="/home" activeClassName={classes.active}>Home</NavLink></li>
-                        <li><NavLink to="/about-us" activeClassName={classes.active}>About Us</NavLink></li>
-                        <li><NavLink to="/contact" activeClassName={classes.active}>Contact Us</NavLink></li>
-                        <li><NavLink to="/menu" activeClassName={classes.active}>Menu</NavLink></li>
-                    </ul>
-                    <button><i className="fas fa-bars fa-2x"></i></button>
+                    {!openNav && <button onClick={openNavHandler}><i className="fas fa-bars fa-2x"></i></button>}
+                    <div className={!openNav ? classes.close : classes.open}>
+                        <ul>
+                            <li><NavLink to="/home" activeClassName={classes.active} onClick={closeMenu}>Home</NavLink></li>
+                            <li><NavLink to="/about-us" activeClassName={classes.active} onClick={closeMenu}>About Us</NavLink></li>
+                            <li><NavLink to="/contact" activeClassName={classes.active} onClick={closeMenu}>Contact Us</NavLink></li>
+                            <li><NavLink to="/menu" activeClassName={classes.active} onClick={closeMenu}>Menu</NavLink></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </Fragment >
