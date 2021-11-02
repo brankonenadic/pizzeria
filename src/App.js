e1aa6fef1ed7de2a3d2ec612862af78bce192be1
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-02 11:25:03 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { Fragment } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 
 function App() {
   return (
-   <Fragment>
-     <Switch>
-      <Route path="/" exact>
-        <Redirect to="/home" />
-      </Route>
-      <Route path="/home">
-        <Home />
-      </Route>
-     </Switch>
-   </Fragment>
+    <Fragment>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/about-us">
+          <AboutUs />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
